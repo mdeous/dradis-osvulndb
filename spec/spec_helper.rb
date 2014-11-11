e4d8d678c90a0581fec6205d764bb5bc929a2d82
Daniel Martin <etd-github@nomejortu.com>
2014-11-11 12:12:11 +0100

    Copy specs/ from main repo

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../../../../config/environment", __FILE__)
+require 'rspec/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+# require 'support/fixture_loader'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  # Use the specified formatter
+  config.formatter = :documentation
+
+  # uncomment to use :focus => true to run a single Spec
+  # config.filter_run :focus => true
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  config.before(:suite) do
+    Configuration.create(:name=>'revision', :value=>'0')
+  end
+end
