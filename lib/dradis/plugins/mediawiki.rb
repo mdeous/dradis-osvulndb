05c27071ffaf5808d181f7ce5dadc2d72b32f077
Daniel Martin <etd-github@nomejortu.com>
2014-11-11 12:13:55 +0100

    Add helper methods to Dradis::Plugins::Mediawiki

--- lib/dradis/plugins/mediawiki.rb
+++ lib/dradis/plugins/mediawiki.rb
@@ -1,6 +1,24 @@
 module Dradis
   module Plugins
     module Mediawiki
+      # class Configuration < Core::Configurator
+      #   configure :namespace => 'wikiimport'
+      #   setting :host, :default => 'localhost'
+      #   setting :port, :default => 80
+      #   setting :path, :default => '/mediawiki/api.php'
+      #   setting :fields, :default => 'Title,Impact,Probability,Description,Recommendation'
+      # end
+
+      # WikiMedia has its own formatting, and there are some tweaks we need to do
+      # to addapt it to standard dradis convention.
+      def self.fields_from_wikitext(wikitext)
+        dradis_fields = wikitext
+        fields = Configuration.fields.split(',')
+        fields.each do |f|
+          dradis_fields.sub!( /=+#{f}=+/, "#[#{f}]#" )
+        end
+        return dradis_fields
+      end
     end
   end
 end
