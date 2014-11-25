80cdaa4e0c1be5c30c5221217d3275d4f5142491
Daniel Martin <etd-github@nomejortu.com>
2014-11-25 13:49:01 +0100

    Thorfile - fix tasks so they can be loaded from the main app

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,39 +1,17 @@
-module Dradis
-  module Plugins
-    module Mediawiki
+class MediawikiTasks < Thor
+  namespace "dradis:plugins:wiki"
 
-      class Thor < Thor
-        namespace "dradis:plugins:wiki"
+  desc "search QUERY", "perform a general search against a remote MediaWiki"
+  def search(query)
+    require 'config/environment'
 
-        desc "search14 QUERY", "perform a general search against a remote MediaWiki v1.14"
-        def search14(query)
-          require 'config/environment'
+    results = Dradis::Plugins::Mediawiki::Filters::FullTextSearch.new.query(query: query)
 
-          results = WikiImport::Filters::FullTextSearch14.run(:query => query)
+    puts "Wiki Search\n==========="
+    puts "#{results.size} results"
 
-          puts "Wiki Search\n==========="
-          puts "#{results.size} results"
-
-          results.each do |record|
-            puts "#{record[:title]}\n\t#{record[:description]}"
-          end
-        end
-
-        desc "search15 QUERY", "perform a general search against a remote MediaWiki v1.15"
-        def search15(query)
-          require 'config/environment'
-
-          results = WikiImport::Filters::FullTextSearch15.run(:query => query)
-
-          puts "Wiki Search\n==========="
-          puts "#{results.size} results"
-
-          results.each do |record|
-            puts "#{record[:title]}\n\t#{record[:description]}"
-          end
-        end
-      
-      end
+    results.each do |record|
+      puts "#{record.title}\n\t#{record.description}"
     end
   end
 end
