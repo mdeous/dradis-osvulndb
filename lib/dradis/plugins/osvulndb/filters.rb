4f5226e88b83a90a2b4d634338a531b7674dcec1
Mathieu Deous <mathieu.deous@datadoghq.com>
2021-08-27 23:42:27 +0200

    update code to support new files vulndb structure

--- lib/dradis/plugins/osvulndb/filters.rb
+++ lib/dradis/plugins/osvulndb/filters.rb
@@ -1,22 +1,32 @@
 module Dradis::Plugins::Osvulndb::Filters
   class Search < Dradis::Plugins::Import::Filters::Base
+    def read_ref(basedir, ref)
+      refpath = ref.gsub(/#\/files\//, "")
+      desc_path = "#{basedir}/#{refpath}.md"
+
+      File.read(desc_path).strip
+    end
+    
     def query(params = {})
       q = params[:query].downcase
       results = []
 
       db_path = Dradis::Plugins::Osvulndb::Engine.settings.db_path
-      Dir.glob("#{db_path}/*.json").each do |db_file|
+      db_lang = Dradis::Plugins::Osvulndb::Engine.settings.db_lang
+      db_basedir = "#{db_path}/db/#{db_lang}"
+      Dir.glob("#{db_basedir}/*.json").each do |db_file|
         data = JSON.parse(File.read(db_file))
         title = data['title']
-        desc = data['description'].join(' ')
+        desc =  read_ref(db_basedir, data['description']['$ref'])
 
         title_match = title.downcase.include?(q)
         desc_match = desc.downcase.include?(q)
         tags_match = data['tags'].nil? ? false : data['tags'].include?(q)
         if title_match or desc_match or tags_match
+          fix = read_ref(db_basedir, data['fix']['guidance']['$ref'])
           results << Dradis::Plugins::Import::Result.new(
             title: title,
-            description: fields_from_json(data)
+            description: fields_from_json(data, desc, fix)
           )
         end
       end
@@ -24,58 +34,47 @@ module Dradis::Plugins::Osvulndb::Filters
       results
     end
 
-    def fields_from_json(data)
-      dradis_fields = "#[Title]#\n#{data['title']}\n\n"
-      
-      if data['description'].kind_of?(Array)
-        dradis_fields += "#[Description]#\n#{data['description'].join(' ')}\n\n"
-      else
-        dradis_fields += "#[Description]#\n#{data['description']}\n\n"
-      end
+    def fields_from_json(vulndata, desc, fix)
+      dradis_fields = "#[Title]#\n#{vulndata['title']}\n\n"
+      dradis_fields += "#[Description]#\n#{desc}\n\n"
+      dradis_fields += "#[Severity]#\n#{vulndata['severity']}\n\n"
+      dradis_fields += "#[Remediation Effort]#\n#{vulndata['fix']['effort']}\n\n"
+      dradis_fields += "#[Fix]#\n#{fix}\n\n"
 
-      dradis_fields += "#[Severity]#\n#{data['severity']}\n\n"
-      dradis_fields += "#[Remediation Effort]#\n#{data['fix']['effort']}\n\n"
-      
-      if data['fix']['guidance'].kind_of?(Array)
-        dradis_fields += "#[Fix]#\n#{data['fix']['guidance'].join(' ')}\n\n"
-      else
-        dradis_fields += "#[Fix]#\n#{data['fix']['guidance']}\n\n"
-      end
-
-      unless data['wasc'].nil?
+      unless vulndata['wasc'].nil?
         wasc = "#[WASC]#\n"
-        data['wasc'].each do |wasc_id|
+        vulndata['wasc'].each do |wasc_id|
           wasc += "* #{wasc_id}\n"
         end
         dradis_fields += "#{wasc}\n"
       end
 
-      unless data['cwe'].nil?
+      unless vulndata['cwe'].nil?
         cwe = "#[CWE]#\n"
-        data['cwe'].each do |cwe_id|
+        vulndata['cwe'].each do |cwe_id|
           cwe += "* \"#{cwe_id}\":https://cwe.mitre.org/data/definitions/#{cwe_id}.html\n"
         end
         dradis_fields += "#{cwe}\n"
       end
 
-      unless data['owasp_top_10'].nil?
+      unless vulndata['owasp_top_10'].nil?
         owasp = "#[OWASP]#\n"
-        data['owasp_top_10'].each do |owasp_id|
+        vulndata['owasp_top_10'].each do |owasp_id|
           owasp += "* #{owasp_id}\n"
         end
         dradis_fields += "#{owasp}\n"
       end
 
-      unless data['references'].nil?
+      unless vulndata['references'].nil?
         refs = "#[References]#\n"
-        data['references'].each do |ref|
+        vulndata['references'].each do |ref|
           refs += "* #{ref['url']}\n"
         end
         dradis_fields += "#{refs}\n"
       end
 
-      unless data['tags'].nil?
-        dradis_fields += "#[Tags]#\n#{data['tags'].join(',')}\n\n"
+      unless vulndata['tags'].nil?
+        dradis_fields += "#[Tags]#\n#{vulndata['tags'].join(',')}\n\n"
       end
 
       dradis_fields.gsub('`', '@')
